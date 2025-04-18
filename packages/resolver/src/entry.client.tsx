8b9366c754d722d22e68986822fc3e95e23f9b14
Jacob Ebey <jacob.ebey@live.com>
2025-04-17 18:29:03 -0700

    fix: add server actions support to hidden entries (#16)

--- packages/resolver/src/entry.client.tsx
+++ packages/resolver/src/entry.client.tsx
@@ -2,31 +2,40 @@
 
 import * as React from "react";
 import { hydrateRoot } from "react-dom/client";
-import { getServerStream, ServerBrowserRouter } from "react-router";
+import {
+  createCallServer,
+  getServerStream,
+  ServerBrowserRouter,
+} from "react-router";
 import type { ServerPayload } from "react-router/server";
-// @ts-expect-error
-import { createFromReadableStream } from "react-server-dom-parcel/client";
+import {
+  createFromReadableStream,
+  encodeReply,
+  setServerCallback,
+  // @ts-expect-error
+} from "react-server-dom-parcel/client";
 
-createFromReadableStream(
-  getServerStream(),
-  { assets: "manifest" },
-  {
-    temporaryReferences: {
-      clientId: () => <div>Client ID</div>,
-    },
-  }
-).then((payload: ServerPayload) => {
-  React.startTransition(() => {
-    hydrateRoot(
-      document,
-      React.createElement(
-        React.StrictMode,
-        null,
-        React.createElement(ServerBrowserRouter, {
-          decode: createFromReadableStream,
-          payload,
-        })
-      )
-    );
-  });
+const callServer = createCallServer({
+  decode: (body) => createFromReadableStream(body, { callServer }),
+  encodeAction: (args) => encodeReply(args),
 });
+
+setServerCallback(callServer);
+
+createFromReadableStream(getServerStream(), { assets: "manifest" }).then(
+  (payload: ServerPayload) => {
+    React.startTransition(() => {
+      hydrateRoot(
+        document,
+        React.createElement(
+          React.StrictMode,
+          null,
+          React.createElement(ServerBrowserRouter, {
+            decode: (body) => createFromReadableStream(body),
+            payload,
+          })
+        )
+      );
+    });
+  }
+);
--- packages/resolver/src/entry.rsc.ts
+++ packages/resolver/src/entry.rsc.ts
@@ -1,15 +1,32 @@
 "use server-entry";
 
-// @ts-expect-error
-import { renderToReadableStream } from "react-server-dom-parcel/server.edge";
-import { matchServerRequest } from "react-router/server";
+import {
+  decodeReply,
+  loadServerAction,
+  renderToReadableStream,
+  // @ts-expect-error
+} from "react-server-dom-parcel/server.edge";
+import {
+  type DecodeCallServerFunction,
+  matchServerRequest,
+} from "react-router/server";
 
 import routes from "virtual:react-router/routes";
 
 import "./entry.client.tsx";
 
+const decodeCallServer: DecodeCallServerFunction = async (actionId, reply) => {
+  const args = await decodeReply(reply);
+  const action = await loadServerAction(actionId);
+  return action.bind(null, ...args);
+};
+
 export async function callServer(request: Request) {
-  const match = await matchServerRequest({ request, routes });
+  const match = await matchServerRequest({
+    decodeCallServer,
+    request,
+    routes,
+  });
 
   return new Response(renderToReadableStream(match.payload), {
     status: match.statusCode,
